d91859b8328f4dad74aa09e0818b660348e35d82
Kagenooi <153362315+Kagenooi@users.noreply.github.com>
2024-10-22 00:55:22 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,79 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+
+    pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
+});
+
+window.onscroll = function () {
+    const header = document.getElementById("header");
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+};
+
+const body = document.querySelector("body");
+const menuButton = document.querySelector(".menu_button");
+const closeButton = document.querySelector(".mobile_menu_close");
+const menu = document.querySelector(".mobile_menu");
+const mobileNavBarItems = document.querySelectorAll(".mobile_nav_bar_item");
+
+mobileNavBarItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        menu.classList.remove("mobile_menu_show");
+        body.classList.remove("overflow_hidden");
+    });
+});
+
+menuButton.addEventListener("click", () => {
+    menu.classList.add("mobile_menu_show");
+    body.classList.add("overflow_hidden");
+});
+
+closeButton.addEventListener("click", () => {
+    menu.classList.remove("mobile_menu_show");
+    body.classList.remove("overflow_hidden");
+});
+
+const generalTerms = document.querySelector(".general_terms_link");
+const privacyPolicy = document.querySelector(".privacy_policy_link");
+const paths = window.location.pathname;
+const page = paths.split("/").pop(); // Gets the last part of the path
+
+
+if (page === "privacy_policy.html") {
+    console.log("You are on the index page.");
+    privacyPolicy.style.color = "#E29723";
+
+} else if (page === "general_terms.html") {
+    generalTerms.style.color = "#E29723";
+} else {
+    console.log("You are on a different page.");
+}
 let parallaxInstance;
 
 const checkParallax = () => {
@@ -24,12 +100,12 @@ window.addEventListener("resize", checkParallax);
 
 const playButton = document.querySelector(".video_play_button");
 const videoPlayer = document.querySelector(".video_player");
-playButton.addEventListener("click", () => {
+playButton?.addEventListener("click", () => {
     videoPlayer.play();
     playButton.classList.add("display_none");
 });
 
-videoPlayer.addEventListener("click", () => {
+videoPlayer?.addEventListener("click", () => {
     videoPlayer.pause();
     playButton.classList.remove("display_none");
 });
